92d0d2356c844521a3ef717bbe3ad81eb03b0ecd
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-06-18 11:51:42 -0500

    update to components content for React course

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 //React component:
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice memes!</div>
-                </div>
-            </div>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
         </div>
     );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
